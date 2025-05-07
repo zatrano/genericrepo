3845e48583d623983e567b26517324f7662bd231
ZATRANO <36309658+zatrano@users.noreply.github.com>
2025-05-07 11:15:49 +0300

    Update auth_repository.go

--- repositories/auth_repository.go
+++ repositories/auth_repository.go
@@ -1,9 +1,11 @@
 package repositories
 
 import (
-	"zatrano/configs/configsdatabase"
-	"zatrano/models"
+	"davet.link/configs/configsdatabase"
+	"davet.link/configs/configslog"
+	"davet.link/models"
 
+	"go.uber.org/zap"
 	"gorm.io/gorm"
 )
 
@@ -21,26 +23,47 @@ func NewAuthRepository() IAuthRepository {
 	return &AuthRepository{db: configsdatabase.GetDB()}
 }
 
-func (r *AuthRepository) FindUserByAccount(account string) (*models.User, error) {
+func (r *AuthRepository) executeQuery(query *gorm.DB, operation string, fields ...zap.Field) error {
+	if err := query.Error; err != nil {
+		fields = append(fields, zap.Error(err))
+		configslog.Log.Error(operation+" hatası", fields...)
+		return err
+	}
+	return nil
+}
+
+func (r *AuthRepository) findUser(query *gorm.DB, operation string, fields ...zap.Field) (*models.User, error) {
 	var user models.User
-	err := r.db.Where("account = ?", account).First(&user).Error
+	err := r.executeQuery(query.First(&user), operation, fields...)
 	if err != nil {
 		return nil, err
 	}
 	return &user, nil
+}
+
+func (r *AuthRepository) FindUserByAccount(account string) (*models.User, error) {
+	return r.findUser(
+		r.db.Where("account = ?", account),
+		"Kullanıcı sorgulama (account)",
+		zap.String("account", account),
+	)
 }
 
 func (r *AuthRepository) FindUserByID(id uint) (*models.User, error) {
-	var user models.User
-	err := r.db.First(&user, id).Error
-	if err != nil {
-		return nil, err
-	}
-	return &user, nil
+	return r.findUser(
+		r.db.Where("id = ?", id),
+		"Kullanıcı sorgulama (ID)",
+		zap.Uint("user_id", id),
+	)
 }
 
 func (r *AuthRepository) UpdateUser(user *models.User) error {
-	return r.db.Save(user).Error
+	return r.executeQuery(
+		r.db.Save(user),
+		"Kullanıcı güncelleme",
+		zap.Uint("user_id", user.ID),
+		zap.String("account", user.Account),
+	)
 }
 
 var _ IAuthRepository = (*AuthRepository)(nil)
